a46e6fce11924b3b41c60b6706f3fa170ef026ba
Nour Al Hassan <Nour.AlHassan@Student.HTW-Berlin.de>
2022-06-06 15:14:37 +0200

    Rest API: Contact

---
+++ src/main/java/de/htwberlin/webtech/web/api/Contact.java
@@ -0,0 +1,69 @@
+package de.htwberlin.webtech.web.api;
+
+public class Contact {
+
+    private int id;
+    private String firstName;
+    private String secondName;
+    private String work;
+
+    public Contact(int id, String firstName, String secondName, String work, String email, String phone) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.work = work;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    private String email;
+    private String phone;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
